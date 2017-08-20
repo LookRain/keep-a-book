0affda07e66c933a90da339b18522f6e504a9f17
LookRain <brantscube@hotmail.com>
2017-08-20 22:26:16 +0800

    test network request, need to set legal domain in wxsettings

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,8 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    fakeData: ''
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,6 +16,25 @@ Page({
   onLoad: function () {
     console.log('onLoad')
     var that = this
+    
+    let uri = 'https://jsonplaceholder.typicode.com/posts/1'
+
+    wx.request({
+      url: uri,
+      header: {
+        'content-type': 'application/json'
+      },
+      data: '',
+
+      success: res => {
+        console.log('haha' + res.statusCode)
+        this.setData({
+          fakeData: JSON.stringify(res.data)
+        })
+      }
+    })
+
+    
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
