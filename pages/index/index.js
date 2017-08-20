082d3f1c33bc77644c7d259729d82f38a03a9fe2
LookRain <brantscube@hotmail.com>
2017-08-20 21:29:12 +0800

    reinit with wx app id

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,7 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hellsdfo World',
+    motto: 'Hello World',
     userInfo: {}
   },
   //事件处理函数
@@ -16,7 +16,7 @@ Page({
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo( userInfo => {
+    app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
         userInfo:userInfo
